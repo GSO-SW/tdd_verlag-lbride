d5cff1edf9d9647243b158700f2b4c1974f3df12
Leander Bride <leander.b4@gso.schule.koeln>
2023-01-12 09:14:10 +0100

    Tests iplementiert

---
+++ Verlag/Buch.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Verlag
+{
+    public class Buch
+    {
+        public string Titel { get; set; }
+
+        private string autor;
+        public string Autor
+        {
+            get => autor;
+            set
+            {
+                autor = Regex.IsMatch(value, @"[#;§%""\0]+") switch
+                {
+                    false => value,
+                    true => throw new ArgumentException("Unerlaubte Zeichen", new FormatException())
+                };
+            }
+        }
+
+
+        private int auflage;
+        public int Auflage
+        {
+            get => auflage;
+            set
+            {
+                auflage = value switch
+                {
+                    < 1 => throw new ArgumentOutOfRangeException(),
+                    _ => value
+                };
+            }
+        }
+
+        public Buch(string autor, string titel, int auflage = 1)
+        {
+            Autor = autor;
+            Titel = titel;
+            Auflage = auflage;
+        }
+    }
+}
